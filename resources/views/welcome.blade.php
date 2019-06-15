0332a0579f12949844dd198bd4fdbead8e4c7520
Magnus <Magnus@p13.dk>
2019-06-15 11:05:43 +0200

    Changed Title to config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config("app.name") }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -87,7 +87,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    EchoFive
+                    {{ config("app.name") }}
                 </div>
                 <div class="links">
                     @if (Route::has('login'))
